b0a05ce350192fa42df8b421cb461b3f72aa54a4
hilbert-flex <cyaa@umich.edu>
2023-10-05 05:07:13 +0000

    chore: turn off debug mode

--- interference/app.py
+++ interference/app.py
@@ -157,7 +157,7 @@ def embeddings():
     }
 
 def main():
-    app.run(host='0.0.0.0', port=1337, debug=True)
+    app.run(host='0.0.0.0', port=1337, debug=False)
 
 if __name__ == '__main__':
     main()
